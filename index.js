86b1551443c65fdaf5a03337ad3d9c2839264c9d
javid-akthar <aktharjavid74@gmail.com>
2022-12-06 18:32:15 +0530

    update

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 const express = require('express');
-const port = 8007;
+const port = process.env.PORT || 3001;
 const fs = require('fs');
 const path = require('path');
 const expressLayout = require('express-ejs-layouts')
